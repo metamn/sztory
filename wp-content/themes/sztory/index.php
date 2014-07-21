13cb61cdd9fa6ab3df6835abd2a58e002cb51943
cs <bartus.csongor@gmail.com>
2014-07-21 17:54:58 +0300

    Atom values are retrieved on the frontend

--- wp-content/themes/sztory/index.php
+++ wp-content/themes/sztory/index.php
@@ -1,25 +1,17 @@
 <?php get_header(); ?>
 
   <?php 
-    if (have_posts()) {
-      while ( have_posts() ) : the_post(); ?>
-      
-        posts here  
-      <?php 
-    endwhile;
-    
-	} else { ?>
-	  
-	  <section id="not-found">
-        <div class="container">
-          <article>
-            <div class="inner">
-              <h1>Not found</h1>
-            </div>
-          </article>
-        </div>
-      </section>
-	  
+    if (have_posts()) { ?>
+      <ul>
+        <?php while ( have_posts() ) : the_post(); ?>
+          <li>
+            <?php the_title() ?>: 
+            <?php echo get_post_meta(get_the_ID(), 'atom_value', true ); ?>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+	<?php } else { ?>
+	  <p>Not found</p>	  
 	<?php } ?>
 
 <?php get_footer(); ?>
