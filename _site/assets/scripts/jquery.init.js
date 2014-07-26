532d62ab8314d5ab774474979f29722c291fa3c7
cs <bartus.csongor@gmail.com>
2014-07-26 12:10:13 +0300

    The structure is outlined well in HTML5

--- _site/assets/scripts/jquery.init.js
+++ _site/assets/scripts/jquery.init.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   
   // Show the first article in the timeline
-  jQuery("#timeline article").first().addClass('active');
+  jQuery("#timeline article.timeline").first().addClass('active');
 
   // Hide duplicated years in the timeline
   for (var i=1989; i<2015; i++) {
-    jQuery("#timeline article .time-" + i).first().addClass('visible');
+    jQuery("#timeline article.timeline .time-" + i).first().addClass('visible');
   }
 
 });
