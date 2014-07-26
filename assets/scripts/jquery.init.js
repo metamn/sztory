75afbabf9f0ec93312578bf8d4a4c810edccb91e
cs <bartus.csongor@gmail.com>
2014-07-26 10:55:55 +0300

    Basic styling added to the timeline sztory

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  
+  // Show the first article in the timeline
+  jQuery("#timeline article").first().addClass('active');
 
   // Hide duplicated years in the timeline
   for (var i=1989; i<2015; i++) {
