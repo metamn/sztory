cb264657974cb5b1c87c65a5e9beaa791ffb7728
cs <bartus.csongor@gmail.com>
2014-08-01 11:20:48 +0300

    Everything removed, just text ...

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
   
-  // Show the first article in the timeline
-  jQuery("#timeline article.timeline").addClass('active');
-
-  // Hide duplicated years in the timeline
-  for (var i=1989; i<2015; i++) {
-    jQuery("#timeline article.timeline .time-" + i).first().addClass('visible');
-  }
-
 });
