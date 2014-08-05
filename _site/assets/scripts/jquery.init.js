9a85403149d6f920c24f926391b639f1d57142bd
cs <bartus.csongor@gmail.com>
2014-08-05 15:01:56 +0300

    The hover effect removed

--- _site/assets/scripts/jquery.init.js
+++ _site/assets/scripts/jquery.init.js
@@ -3,17 +3,8 @@ $(document).ready(function() {
   // Mark the first item in timeline active
   $('#timeline article.timeline').first().addClass('active');
   
-  // On hover show guidance
-  $('#timeline article.timeline .date').hover(
-    function() {
-      $(this).addClass('hover');
-    }, function() {
-      $(this).removeClass('hover');
-    }
-  );
-  
   // Toggle active timeline entry
-  $('#timeline article.timeline .date').click(function() {
+  $('#timeline article.timeline .date, #timeline article.timeline .title').click(function() {
     $(this).parent().toggleClass('active');
   });
 });
