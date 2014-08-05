95f6b53fac494ec8131a246e5868e637f3deed5e
cs <bartus.csongor@gmail.com>
2014-08-05 16:48:00 +0300

    Tags added

--- _site/assets/scripts/jquery.init.js
+++ _site/assets/scripts/jquery.init.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
 
-  // Mark the first item in timeline active
-  $('#timeline article.timeline').first().addClass('active');
   
   // Toggle active timeline entry
   $('#timeline article.timeline .date, #timeline article.timeline .title').click(function() {
     $(this).parent().toggleClass('active');
+  });
+  
+  
+  // Gradient background for timeline articles 
+  $('#timeline article.timeline').each(function(index) {
+    gradient = 250 + index * 75;
+    color = 'hsl(' + gradient.toString() + ', 53%, 88%)';
+    $(this).find('.tags ul li span').css({'border-color' : color, 'background' : color});
   });
 });
