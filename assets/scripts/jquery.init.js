b31c1979562c8cc52d16ee8c38c6ab135c387622
cs <bartus.csongor@gmail.com>
2014-08-05 11:59:20 +0300

    Started to add simple navigation

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+
+  // Mark the first item in timeline active
+  $('#timeline article.timeline').first().addClass('active');
   
+  // Toggle active timeline entry
+  $('#timeline article.timeline .date').click(function() {
+    $(this).parent().toggleClass('active');
+  });
 });
