0f02c9396147519cab96c20d2a3d9ab9465ad695
cs <bartus.csongor@gmail.com>
2014-08-06 13:32:06 +0300

    Playing with scrolling

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
-
+  
+  
+  
+  // Scroll the whole page at once
+  scrollTo($('article.navigation-full-screen').last());
+  
+  // Scroll to an element on the page
+  function scrollTo(element) {
+    $('html, body').animate({
+        scrollTop: element.offset().top
+    }, 2000);
+  }
+  
+  
+  
+  
+  
   // Add background image to articles
   $('article.navigation-full-screen').each(function() {
     var url = $(this).find('img').attr('src');
     $(this).css('background-image', 'url(' + url + ')');
   });
+  
 });
