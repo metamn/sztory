cb498c1147261df40a897d16b617dc911e0e6882
cs <bartus.csongor@gmail.com>
2014-08-06 20:05:31 +0300

    Trying mobile scrolling wo background image

--- _site/assets/scripts/jquery.init.js
+++ _site/assets/scripts/jquery.init.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
   
   
   // Add background image to articles
-  $('article.navigation-full-screen').each(function() {
+  $('article.navigation-full-screen .image').each(function() {
     var url = $(this).find('img').attr('src');
     $(this).css('background-image', 'url(' + url + ')');
   });
